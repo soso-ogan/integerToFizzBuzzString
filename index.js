c3aeafa53da83c6b1af22085348dfb65b4d41bde
Soso <soso@sosos-mbp.home>
2023-01-28 17:38:36 +0000

    Added isFizzBuzz function and remaining if statements for string 'Buzz' and 'FizzBuzz'

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function integerToString(integer) {
   stringArray = [];
   for (let int = 1; int <= integer; int++) {
-    if (isFizz(int)) {
+    if (isFizzBuzz(int)) {
+      stringArray.push("FizzBuzz");
+    } else if (isFizz(int)) {
       stringArray.push("Fizz");
+    } else if (isBuzz(int)) {
+      stringArray.push("Buzz");
     } else {
       stringArray.push(int);
     }
   }
   return stringArray.join(", ");
+}
+
+function isFizzBuzz(number) {
+  return isFizz(number) && isBuzz(number);
 }
 
 function isFizz(number) {
@@ -18,7 +26,4 @@ function isBuzz(number) {
   return number % 5 == 0 ? true : false;
 }
 
-function isFizzBuzz(number) {
-  return isFizz(number) && isBuzz(number);
-}
 module.exports = { integerToString };
--- index.test.js
+++ index.test.js
@@ -3,3 +3,13 @@ const { integerToString } = require("./index");
 test("Should output a string of numbers from 1 to 'n' separated by a comma where if the number is divisible by 3, the number should be replaced with 'Fizz',", () => {
   expect(integerToString(3)).toStrictEqual("1, 2, Fizz");
 });
+
+test("Should output a string of numbers from 1 to 'n' separated by a comma where if the number is divisible by 5, the number should be replaced with 'Buzz',", () => {
+  expect(integerToString(5)).toStrictEqual("1, 2, Fizz, 4, Buzz");
+});
+
+test("Should output a string of numbers from 1 to 'n' separated by a comma where if the number is divisible by 3 and 5, the number should be replaced with 'FizzBuzz',", () => {
+  expect(integerToString(15)).toStrictEqual(
+    "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz"
+  );
+});
