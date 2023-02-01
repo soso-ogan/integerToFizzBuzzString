e31a279be4b8bb6a5c051d79acdebb7ebe29a69b
Soso O <99879159+soso-ogan@users.noreply.github.com>
2023-02-01 10:13:59 +0000

    Removed unnecessary console logs

--- index.js
+++ index.js
@@ -26,7 +26,4 @@ function isBuzz(number) {
   return number % 5 == 0 ? true : false;
 }
 
-console.log(`"${integerToFizzBuzzString(3)}"`);
-console.log(`"${integerToFizzBuzzString(5)}"`);
-console.log(`"${integerToFizzBuzzString(15)}"`);
 module.exports = { integerToFizzBuzzString };
